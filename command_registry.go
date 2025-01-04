c86e30122cc66488e1685240fb7768c95bb325e7
Manoj <manoj19.sinnur@gmail.com>
2025-01-04 23:32:59 +0530

    PokeApi

--- command_registry.go
+++ command_registry.go
@@ -3,6 +3,11 @@ package main
 import (
     "os"
     "fmt"
+    "net/http"
+    "io"
+    "encoding/json"
+
+    // "github.com/mtslzr/pokeapi-go"
 )
 
 type cliCommand struct {
@@ -11,6 +16,20 @@ type cliCommand struct {
     callback        func() error
 }
 var commands map[string]cliCommand
+
+var url = "https://pokeapi.co/api/v2/location-area/"
+var prevUrl string
+type LocationAreaResponse struct {
+    Count int
+    Next string
+    Previous string
+    Results []LocationArea
+}
+
+type LocationArea struct {
+    Name string
+    Url  string
+}
 
 func commandExit() error {
     fmt.Println("Closing the Pokedex... Goodbye!")
@@ -27,6 +46,61 @@ func commandHelp() error {
     return nil
 }
 
+func commandMap() error {
+    res, err := http.Get(url)
+    if err != nil {
+        return err
+    }
+
+    body, err := io.ReadAll(res.Body)
+    if err != nil {
+        return err
+    }
+    defer res.Body.Close()
+
+    var data LocationAreaResponse
+    if json.Valid([]byte(body)){
+        json.Unmarshal([]byte(body), &data)
+    }
+
+    for _, area := range data.Results {
+        fmt.Println(area.Name)
+    }
+    prevUrl = data.Previous
+    url = data.Next
+    return nil
+}
+
+
+func commandMapBack() error {
+    if prevUrl == "" {
+        fmt.Println("No previous location areas to display.")
+    }
+    res, err := http.Get(prevUrl)
+    if err != nil {
+        return err
+    }
+
+    body, err := io.ReadAll(res.Body)
+    if err != nil {
+        return err
+    }
+    defer res.Body.Close()
+
+    var data LocationAreaResponse
+    if json.Valid([]byte(body)){
+        json.Unmarshal([]byte(body), &data)
+    }
+
+    for _, area := range data.Results {
+        fmt.Println(area.Name)
+    }
+    url = data.Next
+    prevUrl = data.Previous
+    return nil
+}
+
+
 func initCommands() {
     commands = map[string]cliCommand{
         "exit": {
@@ -38,6 +112,16 @@ func initCommands() {
             name: "help",
             description: "Displays help message",
             callback: commandHelp,
+        },
+        "map": {
+            name: "map",
+            description: "It displays the names of 20 location areas in the Pokemon world.",
+            callback: commandMap,
+        },
+        "mapb": {
+            name: "mapb(map back)",
+            description:  "Displays previous 20 location areas in the Pokemon world.",
+            callback: commandMapBack,
         },
     }
 }
